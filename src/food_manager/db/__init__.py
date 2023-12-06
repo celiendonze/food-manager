d3a8f44c117e825994cc97cc3c0044cc19626881
Célien Donzé <celien.donze@gmail.com>
2023-12-06 22:50:25 +0100

    Put engine in memory if .env not set

--- src/food_manager/db/__init__.py
+++ src/food_manager/db/__init__.py
@@ -8,8 +8,11 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 database_url = os.getenv("DATABASE_URL")
-engine = create_engine(database_url, connect_args={"check_same_thread": False})
-# in memory database
-# engine = create_engine("sqlite:///:memory:", connect_args={"check_same_thread": False})
+if database_url is not None:
+    engine = create_engine(database_url, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(
+        "sqlite:///:memory:", connect_args={"check_same_thread": False}
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
